45c4d2bcbcf4122d14cb9762ff5b24321b8e0ec4
filipussetioo <filipus54@gmail.com>
2024-07-02 15:55:53 +0700

    updating tvshows page

--- app/tvshows/page.tsx
+++ app/tvshows/page.tsx
@@ -3,6 +3,7 @@ import Nav from "../layout/nav/page";
 import {fetchPopularTVShowsData} from "../api/movie.api";
 import Carousel from '../layout/home/component/Carousel';
 import { EmblaOptionsType } from 'embla-carousel'
+import Hero from '../layout/home/component/Hero';
 
 export default async function TVShows() {
     const OPTIONS: EmblaOptionsType = { loop: true }
@@ -10,11 +11,14 @@ export default async function TVShows() {
     const TvResults = tv['results'];
 
     return (
-        <main className="relative min-h-screen bg-white">
+        <main className="relative min-h-screen">
             <Nav/>
-            <section className='h-screen pt-20'>
-                <h1 className='text-3xl'>Popular TV Shows</h1>
-                <Carousel slides={TvResults} options={OPTIONS}/>
+            <section className='h-screen w-screen'>
+                <Hero image={TvResults[0].backdrop_path} />
+                <div className='-top-[5rem] relative px-5'>
+                    <h1 className='text-xl text-white font-bold home-title'>Popular TV Shows</h1>
+                    <Carousel slides={TvResults} options={OPTIONS}/>
+                </div>
             </section>
         </main>
     )
