197989dd57778af45c663305fc3b0284c8b886f9
filipussetioo <filipus54@gmail.com>
2024-07-03 00:24:24 +0700

    fixing api and tvshows logo

---
+++ app/api/movie.api.ts
@@ -0,0 +1,66 @@
+const API_KEY = process.env.API_KEY;
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
+
+export async function fetchData(search:string,type:string,x?:string) {
+    try{
+      const response = await fetch(`https://api.themoviedb.org/3/${search}/${type}${x}language=en-US&page=1`, options);
+      const data = response.json();
+  
+      return data;
+    }catch(error){
+      console.log("Error fetching data:"+ error);
+      const data = "Error fetching data:"+ error
+
+      return data;
+    }
+}
+
+
+export async function fetchRecommendedTVShows() {
+  try{
+    const response = await fetch('https://api.themoviedb.org/3/tv/1434/recommendations?language=en-US&page=1', options);
+    const data = response.json();
+
+    return data;
+  }catch(error){
+    console.log("Error fetching data:"+ error);
+    const data = "Error fetching data:"+ error
+
+    return data;
+  }
+}
+
+export async function fetchRecommendedMovie(){
+  try{
+    const response = await fetch('https://api.themoviedb.org/3/movie/519182/recommendations?language=en-US&page=1', options);
+    const data = response.json();
+
+    return data;
+  }catch(error){
+    console.log("Error fetching data:"+ error);
+    const data = "Error fetching data:"+ error
+
+    return data;
+  }
+}
+
+export async function fetchLogo(movieid:string){
+  try{
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${movieid}/images`, options);
+    const data = response.json();
+
+    return data;
+  }catch(error){
+    console.log("Error fetching data:"+ error);
+    const data = "Error fetching data:"+ error
+
+    return data;
+  }
+}
--- app/tvshows/page.tsx
+++ app/tvshows/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Nav from "../layout/nav/page";
-import {fetchData} from "../api/movie.api";
+import {fetchData, fetchLogo} from "../api/movie.api";
 import Carousel from '../layout/home/component/Carousel';
 import { EmblaOptionsType } from 'embla-carousel'
 import Hero from '../layout/home/component/Hero';
@@ -8,12 +8,13 @@ import Hero from '../layout/home/component/Hero';
 export default async function TVShows() {
     const OPTIONS: EmblaOptionsType = { loop: true }
     const tv = await fetchData('tv','popular?');
+    const popTVid = await fetchLogo(tv['results'][0].id);
 
     return (
         <main className="relative min-h-screen">
             <Nav/>
             <section className='h-screen w-screen'>
-                <Hero image={tv['results'][0].backdrop_path} />
+                <Hero image={tv['results'][0].backdrop_path} logo={popTVid['logos'][0].file_path} />
                 <div className='-top-[5rem] relative px-5'>
                     <h1 className='text-xl text-white font-bold home-title'>Popular TV Shows</h1>
                     <Carousel slides={tv['results']} options={OPTIONS}/>
